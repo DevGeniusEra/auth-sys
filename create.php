e2c2a4ea39ee4d8c9394728bfab8e8ead16f904c
Enterprise-Flex-SaaS-Platform <erfanimohadeseh@gmail.com>
2024-03-17 09:11:43 +0400

    Inserting Comments

--- create.php
+++ create.php
@@ -1,34 +1,34 @@
-<?php 
-  require "includes/header.php"; 
-  require "config.php"; 
-  if(!isset($_SESSION['username'])){
-    header("location: index.php");
-  }
+<?php
+require "includes/header.php";
+require "config.php";
+if (!isset($_SESSION['username'])) {
+  header("location: index.php");
+}
 ?>
 
 <?php
-  if (isset($_POST['submit'])){
-    if ($_POST['title'] == '' OR $_POST['body'] == ''){
-      echo "Some Inputs Are Empty";
-    } else {
-      $title = $_POST['title'];
-      $body = $_POST['body'];
-      $username = $_SESSION['username'];
+if (isset($_POST['submit'])) {
+  if ($_POST['title'] == '' or $_POST['body'] == '') {
+    echo "Some Inputs Are Empty";
+  } else {
+    $title = $_POST['title'];
+    $body = $_POST['body'];
+    $username = $_SESSION['username'];
 
-      $insert = $conn->prepare("INSERT INTO posts (title, body, username) VALUES (:title, :body, :username)");
-      $insert->execute([
-        ':title' => $title,
-        ':body' => $body,
-        ':username' => $username,
-      ]);
-      header("location: index.php");
-    }
+    $insert = $conn->prepare("INSERT INTO posts (title, body, username) VALUES (:title, :body, :username)");
+    $insert->execute([
+      ':title' => $title,
+      ':body' => $body,
+      ':username' => $username,
+    ]);
+    header("location: index.php");
   }
+}
 ?>
 
 <main class="form-signin w-50 m-auto">
   <form method="POST" action="create.php">
-   
+
     <h1 class="h3 mt-5 fw-normal text-center">Create Post</h1>
 
     <div class="form-floating">
@@ -38,7 +38,7 @@
 
     <div class="form-floating mt-4">
       <textarea rows="9" name="body" class="form-control" id="floatingTextarea" placeholder="body"></textarea>
-      <label for="floatingTextarea">Body</label>  
+      <label for="floatingTextarea">Body</label>
     </div>
     <button name="submit" class="w-100 btn btn-lg btn-primary mt-5" type="submit">Create post</button>
   </form>
--- show.php
+++ show.php
@@ -1,21 +1,56 @@
-<?php 
-  require "includes/header.php"; 
-  require "config.php"; 
-  if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    $post = $conn->prepare("SELECT * FROM posts WHERE id = :id");
-    $post->execute([':id' => $id]);
+<?php
+require "includes/header.php";
+require "config.php";
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  $post = $conn->prepare("SELECT * FROM posts WHERE id = :id");
+  $post->execute([':id' => $id]);
 
-    $posts = $post->fetch(PDO::FETCH_OBJ);
-  }
+  $posts = $post->fetch(PDO::FETCH_OBJ);
+}
 ?>
 <main class="row">
-    <div class="card mt-5">
-        <div class="card-body">
-            <h5 class="card-title"><?php echo $posts->title; ?></h5>
-            <p class="card-text"><?php echo $posts->body; ?></p>
-        </div>
+  <div class="card mt-5">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $posts->title; ?></h5>
+      <p class="card-text"><?php echo $posts->body; ?></p>
     </div>
+  </div>
 </main>
 
+<div class="row">
+  <form method="POST" id="comment_data">
+
+    <div class=" form-floating">
+      <input name="username" type="hidden" value="<?php echo $_SESSION['username']; ?>" class="form-control" id="username">
+    </div>
+
+    <div class="form-floating">
+      <input name="post_id" type="hidden" value="<?php echo $posts->id; ?>" class="form-control" id="post_id">
+    </div>
+
+    <div class="form-floating mt-4">
+      <textarea rows="9" name="comment" class="form-control" id="comment" placeholder="body"></textarea>
+      <label for="floatingTextarea">Comment</label>
+    </div>
+    <button name="submit" id="submit" class="w-100 btn btn-lg btn-primary mt-5" type="submit">Create comment</button>
+  </form>
+</div>
 <?php require "includes/footer.php"; ?>
+<script>
+  $(document).ready(function() {
+    $(document).on('submit', function(e) {
+      // alert('form submited');
+      var formdata = $("#comment_data").serialize() + '&submit=submit';
+      $.ajax({
+        type: 'post',
+        url: 'insert-comments.php',
+        data: formdata,
+        success: function() {
+          // alert('success');
+          $("#Comment").text(null);
+        }
+      })
+    })
+  });
+</script>
