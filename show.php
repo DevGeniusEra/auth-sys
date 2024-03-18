df240bc6c149c2360a3c8cd141e1a40b7bf0e010
Enterprise-Flex-SaaS-Platform <erfanimohadeseh@gmail.com>
2024-03-18 09:10:36 +0400

    Showing Comments

--- show.php
+++ show.php
@@ -8,15 +8,19 @@ if (isset($_GET['id'])) {
 
   $posts = $post->fetch(PDO::FETCH_OBJ);
 }
+$comments = $conn->prepare("SELECT * FROM comments WHERE post_id = :post_id");
+$comments->execute([':post_id' => $id]);
+
+$comments = $comments->fetchAll(PDO::FETCH_OBJ);
 ?>
-<main class="row">
+<div class="row">
   <div class="card mt-5">
     <div class="card-body">
       <h5 class="card-title"><?php echo $posts->title; ?></h5>
       <p class="card-text"><?php echo $posts->body; ?></p>
     </div>
   </div>
-</main>
+</div>
 
 <div class="row">
   <form method="POST" id="comment_data">
@@ -35,22 +39,39 @@ if (isset($_GET['id'])) {
     </div>
     <button name="submit" id="submit" class="w-100 btn btn-lg btn-primary mt-5" type="submit">Create comment</button>
   </form>
+  <div id="msg" class="nothing"></div>
+</div>
+<div class="row">
+  <?php foreach ($comments as $singleComment) : ?>
+    <div class="card mt-5">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $singleComment->username; ?></h5>
+        <p class="card-text"><?php echo $singleComment->comment; ?></p>
+      </div>
+    </div>
+  <?php endforeach; ?>
 </div>
 <?php require "includes/footer.php"; ?>
 <script>
   $(document).ready(function() {
-    $(document).on('submit', function(e) {
-      // alert('form submited');
-      var formdata = $("#comment_data").serialize() + '&submit=submit';
+    $('#comment_data').on('submit', function(e) {
+      e.preventDefault(); // Prevent the default form submission
+      var formdata = $(this).serialize() + '&submit=submit';
       $.ajax({
         type: 'post',
         url: 'insert-comments.php',
         data: formdata,
         success: function() {
-          // alert('success');
-          $("#Comment").text(null);
+          $('#comment').val(''); // Clear the comment textarea
+          $('#msg').html('Added Successfully').addClass('alert alert-success bg-success text-white mt-3');
+          fetch();
         }
-      })
-    })
+      });
+    });
   });
+  function fetch(){
+    setInterval(function(){
+      $("body").load("show.php?id=<?php echo$_GET['id']; ?>")
+    }, 4000);
+  }
 </script>
